58bc5dc7a066c4177496a073626ca0101f604a4d
Peter-St <stoiber.peter@aon.at>
2020-12-27 17:50:19 +0100

    BugFix

--- app/build/generated/source/buildConfig/release/humer/UvcCamera/BuildConfig.java
+++ app/build/generated/source/buildConfig/release/humer/UvcCamera/BuildConfig.java
@@ -7,6 +7,6 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "humer.uvc_camera";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 74;
-  public static final String VERSION_NAME = "1.7.7";
+  public static final int VERSION_CODE = 75;
+  public static final String VERSION_NAME = "1.7.8";
 }
