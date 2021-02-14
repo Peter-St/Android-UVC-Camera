77359c574e6c72ac8bda26d0932c82f2edb48504
Peter-St <stoiber.peter@aon.at>
2021-02-14 15:59:13 +0100

    update

--- app/build/intermediates/ndkBuild/release/obj/local/x86_64/objs/sam3u_benchmark/__/__/examples/sam3u_benchmark.o.d
+++
@@ -1,8 +0,0 @@
-/home/peter/Downloads/Webcam/Android-UVC-Camera/app/build/intermediates/ndkBuild/release/obj/local/x86_64/objs/sam3u_benchmark/__/__/examples/sam3u_benchmark.o: \
-  /home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libusb/android/jni/../../examples/sam3u_benchmark.c \
-  /home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libusb/android/jni/../config.h \
-  /home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libusb/android/jni/../../libusb/libusb.h
-
-/home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libusb/android/jni/../config.h:
-
-/home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libusb/android/jni/../../libusb/libusb.h:
