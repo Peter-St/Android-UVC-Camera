83695a415cdb9cf30d754be4a878a6d12267d6a1
Peter-St <stoiber.peter@aon.at>
2021-02-14 16:13:44 +0100

    update

--- app/build/intermediates/ndkBuild/release/obj/local/arm64-v8a/objs/yuv_static/source/row_mmi.o.d
+++
@@ -1,8 +0,0 @@
-/home/peter/Downloads/Webcam/Android-UVC-Camera/app/build/intermediates/ndkBuild/release/obj/local/arm64-v8a/objs/yuv_static/source/row_mmi.o: \
-  /home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libyuv/source/row_mmi.cc \
-  /home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libyuv/include/libyuv/row.h \
-  /home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libyuv/include/libyuv/basic_types.h
-
-/home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libyuv/include/libyuv/row.h:
-
-/home/peter/Downloads/Webcam/Android-UVC-Camera/app/src/main/jni/libyuv/include/libyuv/basic_types.h:
